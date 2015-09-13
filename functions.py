6c3f7d15041ef70e96875a41b17302309c60a297
Alexander Jerneck <alexander.jerneck@gmail.com>
2015-09-13 14:10:33 -0400

    moved to using a jupyther notebook.

--- functions.py
+++ functions.py
@@ -1,19 +1,5 @@
-"""
-Script to explore recipe data.
-"""
-
-import lda
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 import pandas as pd
-import psycopg2
-import seaborn as sns
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction import text
-
-
-
 
 ##TODO: make some simple plots: word counts etc.
 def feature_counts(vectorizer, dtm):
@@ -53,6 +39,34 @@ def get_stop_words():
     return text.ENGLISH_STOP_WORDS.union(
         ingredient_stop_words, instructions_stop_words)
 
+def most_probable_words(model, vocabulary, num_words):
+    """
+    Return a DataFrame of the most probable words for each topic,
+    given a model, vocabulary, and number of words.
+    """
+    ## create array of vocabulary, sorted by topic
+    ## probabilities, one row for each topic.
+    vocab = np.asarray(vocabulary)[np.argsort(model.topic_word_)]
+    wp = np.sort(model.topic_word_)
+
+    ## select n most probable words, which are the right-most
+    ## columns in the vocab array.
+    words = vocab[:, -num_words:-1]
+
+    words = pd.DataFrame(words.T)
+    words['rank'] = words.index
+    words = pd.melt(words, id_vars='rank')
+
+    word_probs = wp[:, -num_words:-1]
+    word_probs = pd.DataFrame(word_probs.T)
+    word_probs['rank'] = word_probs.index
+    word_probs = pd.melt(word_probs, id_vars='rank')
+
+    ww = words.merge(word_probs, on=['rank', 'variable'])
+
+    ww.columns = ['rank', 'label', 'word', 'prob']
+    return ww
+
 
 
 def make_week1_plot(df):
@@ -89,93 +103,4 @@ def make_week1_plot(df):
 
     plt.tight_layout()
     plt.savefig('fig-word-count-histograms.png')
-
-
-## extras.
-# ## plot of most common words (nr of documents).
-# p2 = wc.sort('nr_docs').tail(20).plot('word','nr_docs', kind='bar')
-
-# ## correlation between total count and number of documents:
-# np.corrcoef(wc['count'], wc['nr_docs'])
-
-
-import lda
-
-vectorizer = CountVectorizer(stop_words='english',
-                                 ngram_range=(1, 1),
-                                 token_pattern='[A-Za-z]+')
-features = vectorizer.fit_transform(df.ingredient_txt)
-## features is a document x term matrix.
-
-wc = feature_counts(vectorizer, features)
-
-
-m = lda.LDA(n_topics=40, random_state=0, n_iter=100)
-m.fit(features)
-
-## TODO: make convergence plot.
-
-## 1. TODO: extract topic model assignments.
-
-## topic numbers:
-m.doc_topic_.argsort(axis=1)
-## topic probs:
-np.sort(m.doc_topic_, axis=1)
-
-## data frame of:
-## document id, topic, probability.
-
-# ## this is for the most probable topic, but we don't need it now.
-# x = pd.DataFrame(m.doc_topic_.argsort(axis=1))
-# xm = pd.melt(x)
-# xm.columns = ['topic','']
-
-x = pd.DataFrame(np.sort(m.doc_topic_, axis=1))
-x['doc'] = x.index
-xm = pd.melt(x, id_vars=['doc'])
-xm.columns = ['doc','topic','prob']
-
-## show distribution of document probs by each topic.
-
-I AM HERE: get this plot of one topic distribution to work, then make a grid.
-
-sns.distplot(xm[xm['topic']==0])
-
-
-
-I AM HERE: 1: melt this, to get a 'topic' column from the topic columns
-2: merge with df made from np.sort(m.doc_topic_)
-
-
-
-
-np.arange(9), np.sort(m.doc_topic_, axis=1)[20,-10:-1]
-
-## this shows the most probable topics for a specific document.
-plt.bar(np.arange(9), np.sort(m.doc_topic_, axis=1)[20,-10:-1])
-
-
-plt.pcolor(m.doc_topic_)
-
-
-import seaborn as sns
-
-
-sns.distplot(m.doc_topic_[:,20])
-
-
-np.sort(m.doc_topic_, axis=1)[:,20]
-
-sns.distplot(m.doc_topic_[1,:], kde=False)
-
-
-
-
-
-
-## 2. TODO: make the website which shows one/three recipes from each
-
-## 3. TODO: process optimization (need frontend to evaluate.)
-
-## 4. TODO: run the topic model using stan.
 
---
+++ imports.py
@@ -0,0 +1,10 @@
+import lda
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import psycopg2
+import seaborn as sns
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction import text
+
