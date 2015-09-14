5fdbd167fbe40ff1e1914d6f9f2b3bb02620f757
Alexander Jerneck <alexander.jerneck@gmail.com>
2015-09-14 19:35:44 -0400

    Scraping one recipe and creating dataframe works.

--- scrape.py
+++ scrape.py
@@ -2,7 +2,9 @@ import bs4
 import collections
 import itertools
 import urllib
+import re
 import requests
+import pandas as pd
 
 def make_soup(url):
     "Make a beautiful soup from an url."
@@ -21,7 +23,6 @@ def make_soup(url):
 
 # TODO: generate search urls
 BASE_URL = "http://allrecipes.com/recipes/80/main-dish/?page=%s"
-main_dish_pages = [BASE_URL %  n  for n in range(1, 717)]
 
 # TODO: scrape search result pages.
 
@@ -43,11 +44,51 @@ def parse_recipe_urls(url):
 
     return recipes
 
-main_dishes = map(parse_recipe_urls, main_dish_pages[0:10])
+def parse_recipe(recipe):
+    print(recipe['url'])
+    soup = make_soup(recipe['url'])
+    recipe['ingredients'] = [x.text for x in soup.find_all('span', class_='recipe-ingred_txt added')]
+    recipe['prep_time'] = soup.find(itemprop='prepTime').attrs['datetime']
+    recipe['cook_time'] = soup.find(itemprop='cookTime').attrs['datetime']
+    recipe['total_time'] = soup.find(itemprop='totalTime').attrs['datetime']
+    recipe['calories'] = soup.find(class_='calorie-count').text
+
+    ## nutrients
+    ## TODO: parse nutrients.
+    ## all_nutrients = soup.find_all(class_='nutrientLine')
+
+    ## ratings
+    raw_ratings = soup.find_all(title=re.compile('[0-9]+ cooks'))
+    ratings = map(lambda x: int(x.split()[0]), [x.attrs['title'] for x in raw_ratings])
+    recipe['ratings'] = dict(zip(range(5, 0, -1), ratings))
+    recipe['reviews_nr'] = int(soup.find(class_='recipe-reviews__header--count').text)
+
+    return recipe
+
+## TODO: use full lists, not ranges.
+main_dish_pages = [BASE_URL %  n  for n in range(1, 717)]
+main_dishes_search_results = map(parse_recipe_urls, main_dish_pages[::100])
+## flatten the nested list of search results.
+main_dishes_search_results = list(itertools.chain.from_iterable(main_dishes_search_results))
+
+main_dishes_recipes = map(parse_recipe, main_dishes_search_results[::10])
+
+## build data frame.
+## ingredients:
+[' '.join(x['ingredients']) for x in main_dishes_recipes]
+
+## create separate dataframes for different attributes.
+ratings = pd.DataFrame([x['ratings'] for x in main_dishes_recipes])
+ratings.columns = ['rating_level_%s' % i for i in range(1,6)]
+ingredients = pd.DataFrame({'ingredients':'\n'.join(x['ingredients'])} for x in main_dishes_recipes)
+
+keys =  ['total_time', 'cook_time', u'url', 'calories', u'data-name', u'data-id', u'data-imageurl', 'prep_time', 'reviews_nr']
+metadata = pd.DataFrame([{key: x[key] for key in keys} for x in main_dishes_recipes])
 
 
+df = metadata.join(ingredients).join(ratings)
 
-# TODO: scrape recipe page.
+
 
 # TODO: add more categories.
 
