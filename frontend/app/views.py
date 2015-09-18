3c45b451f4f82300d4d83ebba3cf5a8b6dc31b70
Alexander Jerneck <alexander.jerneck@gmail.com>
2015-09-18 11:08:10 -0400

    Very basic compact view works.

--- frontend/app/views.py
+++ frontend/app/views.py
@@ -11,7 +11,7 @@ def index():
 
     with con:
         cur = con.cursor()
-        cur.execute("SELECT doc_prob.topic, ingredient_txt, image, url, clean_recipes.title, prob FROM doc_prob, clean_recipes WHERE doc_prob.recipe_key=clean_recipes.key ORDER BY topic, rank;")
+        cur.execute("SELECT doc_prob.topic, ingredient_txt, image, url, clean_recipes.title, prob FROM doc_prob, clean_recipes WHERE doc_prob.recipe_key=clean_recipes.key ORDER BY topic desc, rank;")
         query_results = cur.fetchall()
         topics = collections.defaultdict(list)
         for row in query_results:
@@ -44,6 +44,37 @@ def topics():
         for row in doc_probs:
             doc_data[row[0]] += [{'ingredient': row[1], 'image':row[2], 'url':row[3], 'title':row[4]}]
 
-        topics = doc_data.keys()
+        topics = sorted(doc_data.keys(), reverse=True)
 
     return render_template('topics.html', word_data=word_data, doc_data=doc_data, topics=topics)
+
+@app.route('/compact')
+def compact():
+    con = psycopg2.connect(host='localhost', dbname='explore', user='explore', password='Ln2bOYAVCG6utNUSaSZaIVMH')
+
+    with con:
+        cur = con.cursor()
+
+
+        ## extract most probable words for each topic.
+        cur.execute('SELECT * FROM word_probs order by topic, prob desc;')
+        word_probs = cur.fetchall()
+        word_data = collections.defaultdict(list)
+        for row in word_probs:
+            word_data[row[2]] += [row[3]]
+        for k,v in word_data.items():
+            word_data[k] = ', '.join(v)
+
+        ## extract most probable documents for each topic.
+        cur.execute("SELECT doc_prob.topic, ingredient_txt, image, url, clean_recipes.title, prob FROM doc_prob, clean_recipes WHERE doc_prob.recipe_key=clean_recipes.key ORDER BY topic, rank;")
+        doc_probs = cur.fetchall()
+
+        doc_data = collections.defaultdict(list)
+        for row in doc_probs:
+            doc_data[row[0]] += [{'ingredient': row[1], 'image':row[2], 'url':row[3], 'title':row[4]}]
+
+        topics = sorted(doc_data.keys(), reverse=True)
+
+    return render_template('compact.html', word_data=word_data, doc_data=doc_data, topics=topics)
+
+
