1fb742bda9aed3a9311c84ff6cdee6fd49e8fe99
Alexander Jerneck <alexander.jerneck@gmail.com>
2015-09-22 14:14:50 -0400

    Trying the carousel version.

--- frontend/app/views.py
+++ frontend/app/views.py
@@ -104,7 +104,7 @@ def compact():
 
         topics = sorted(doc_data.keys(), reverse=True)
 
-    print topics
+    print doc_data
     return render_template('compact.html', word_data=word_data, doc_data=doc_data, topics=topics)
 
 @app.route('/lucky')
@@ -125,6 +125,48 @@ def lucky():
     return render_template('lucky.html', recipes=gen_recipes)
 
 
-@app.route('/grid')
-def grid():
-    return render_template('grid.html')
+@app.route('/all')
+def all():
+    custom = True
+
+    con = psycopg2.connect(host='localhost', dbname='explore', user='explore', password='Ln2bOYAVCG6utNUSaSZaIVMH')
+
+    engine = create_engine("postgresql+psycopg2://explore:Ln2bOYAVCG6utNUSaSZaIVMH@localhost/explore")
+
+    with con:
+        cur = con.cursor()
+
+        if custom is True:
+            ww = pd.read_sql_table('all_word_probs', engine)
+            xx = ww.sort(['label','prob']).groupby('label').tail(10)
+            word_data = collapse_topic_words(xx)#
+
+        else:
+
+        ## extract most probable words for each topic.
+            cur.execute('SELECT * FROM word_probs order by topic, prob desc;')
+            word_probs = cur.fetchall()
+            word_data = collections.defaultdict(list)
+            for row in word_probs:
+                word_data[row[2]] += [row[3]]
+                for k,v in word_data.items():
+                    cur.execute('SELECT * FROM all_word_probs order by label, prob desc;')
+                    word_probs = cur.fetchall()
+                    word_data[k] = ', '.join(v)
+
+
+        ## extract most probable documents for each topic.
+        cur.execute("SELECT doc_prob.topic, ingredient_txt, image, url, clean_recipes.title, prob FROM doc_prob, clean_recipes WHERE doc_prob.recipe_key=clean_recipes.key ORDER BY topic, rank;")
+        doc_probs = cur.fetchall()
+
+        doc_data = collections.defaultdict(list)
+        for row in doc_probs:
+            doc_data[row[0]] += [{'ingredient': row[1].decode('ascii', 'ignore'), 'image':row[2], 'url':row[3], 'title':row[4].decode('ascii', 'ignore')}]
+
+        topics = sorted(doc_data.keys(), reverse=True)
+
+    print topics
+    mid_point = len(topics)/2
+    print doc_data
+    return render_template('all.html', word_data=word_data, doc_data=doc_data, topics=topics, mid_point=mid_point)
+
