39f9acbf26c46b2c3a15f8d7b86212065a1557b2
Alexander Jerneck <alexander.jerneck@gmail.com>
2015-10-02 15:38:35 -0400

    Make bb run.py correspond to aws run.py
    Otherwise, this causes endless pain when scp -r frontend to update the
    app on aws.

--- frontend/run.py
+++ frontend/run.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python
+# from flask import Flask
+# app = Flask(__name__)
 from app import app
-app.run(debug = True)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
